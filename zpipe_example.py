c0f2414a47fbd85da8fcf48b51e44628ef1b59c7
Lily Chung <lkdc@mit.edu>
2017-07-02 21:44:33 -0400

    remove commented code

--- zpipe_example.py
+++ zpipe_example.py
@@ -6,14 +6,12 @@ from zpipe import ZPipe, Zephyrgram
 
 def main():
     zp = ZPipe(['./zpipe'], print)
-    # zp = ZPipe(['bash', '-c', 'hd 1>&2'], print)
     zp.subscribe('zpipe-example', 'example')
     for i in range(3):
         text = input('enter a message ({}/3) > '.format(i + 1))
         zp.zwrite(Zephyrgram(None, 'zpipe-example', 'example', None,
                              'zpipe-example', True,
                              ['zpipe example {}/3'.format(i + 1), text]))
-    # import time; time.sleep(1)
     zp.close_zephyr()
     return 0
 
